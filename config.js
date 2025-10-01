4a094045530e974a55802bc01e435a3f2f860654
Michaelai1 <michaelcareyai@gmail.com>
2025-09-30 20:27:20 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-// config.js - Configuration file for webhook URL
-// This file should NOT be committed to GitHub (add to .gitignore)
-const CONFIG = {
-    webhookUrl: 'https://michaelcarey.app.n8n.cloud/webhook/ecf4178f-5f34-49ea-baea-410b932c80c7'
-}; 
