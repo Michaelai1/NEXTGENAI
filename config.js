3de001016685921a4092697fb1f3a694a644cd95
Michaelai1 <michaelcareyai@gmail.com>
2025-09-30 20:36:26 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js - Configuration file for webhook URL
+// This file should NOT be committed to GitHub (add to .gitignore)
+const CONFIG = {
+    webhookUrl: 'https://michaelcarey.app.n8n.cloud/webhook/822ce69f-20bd-49f9-8726-cadbe3c4c7ec'
+}; 
